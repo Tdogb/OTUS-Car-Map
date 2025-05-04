a6ad86ee5e1fe59401be9dbf27dec077911a5717
Tdogb <tdogbeard@gmail.com>
2025-05-03 19:14:41 -0500

    working map

--- renderer.js
+++ renderer.js
@@ -34,22 +34,79 @@ const client = redis.createClient();
 
 const map = L.map('map').setView([34.580165, -99.491667], 13);
 
+var terrainLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://opentopomap.org/copyright">OpenTopoMap</a>'
+});
+
 // Set up the tile layer (this is the background map; here we use OpenStreetMap tiles).
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
- // Replace this with your own Mapbox API key
-// const accessToken = 'your_mapbox_api_key_here';
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
 
-// // Initialize the map and set its view to the desired location
-// const map = L.map('map').setView([37.7749, -122.4194], 13); // Centered on San Francisco
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
 
-// // Use Mapbox's satellite tiles
-// L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`, {
-//     attribution: '&copy; <a href="https://www.mapbox.com/">Mapbox</a> contributors',
-//     maxZoom: 19
-// }).addTo(map);
+var Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'jpg'
+});
+
+var elevationLayer = L.tileLayer.wms(
+    'https://elevation.nationalmap.gov/arcgis/services/3DEPElevation/ImageServer/WMSServer?',{
+        layers: '3DEPElevation:Hillshade Gray',
+        className: 'lidarBase',
+});
+
+elevationLayer.on('tileload', function(event) {
+    console.log("tile loaddd");
+    console.log(event);
+    var tile = event.tile;
+    var ctx = tile.getContext('2d');
+    var imageData = ctx.getImageData(0, 0, tile.width, tile.height);
+    
+    for (var i = 0; i < imageData.data.length; i += 4) {
+        var elevationValue = imageData.data[i]; // Assume grayscale elevation
+        var color = getColor(elevationValue);
+        var rgb = L.color(color).rgb();
+        imageData.data[i] = rgb.r;
+        imageData.data[i + 1] = rgb.g;
+        imageData.data[i + 2] = rgb.b;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+});
+
+function getColor(d) {
+    return d > 3000 ? '#008000' : // Green for high elevations
+           d > 1000 ? '#FFFF00' : // Yellow for mid elevations
+           d > 0 ? '#FF0000' :    // Red for low elevations
+                    '#FFFFFF';      // White for no data
+}
+
+// Function to switch between layers
+var baseLayers = {
+    "OpenStreetMap": osmLayer,
+    "Terrain Map": OpenTopoMap,
+    "Stadia Satellite": Stadia_AlidadeSatellite,
+    "Satellite": Esri_WorldImagery,
+    "LIDAR": elevationLayer
+};
+
+// Add layer control
+L.control.layers(baseLayers).addTo(map);
+
+
+
+
 addVehicleMarkers();
 document.getElementById('center-button').onclick = function(){
     let bounds = L.latLngBounds(latlngarray);
@@ -72,10 +129,12 @@ var marked_vehicles = [];
 function addVehicleMarkers() {
     vehicle_id_list.forEach(vehicle_id => {
         if (!marked_vehicles.includes(vehicle_id)) {
-            vehicle = vehicles_dict[vehicle_id.toString()]; 
-            vehicleMarkers[vehicle_id.toString()] = L.marker(vehicle.coords, { icon: vehicle.icon }).addTo(map)
-            .bindPopup('<b>' + vehicle.name + '</b><br>Location: ' + vehicle.coords[0] + ', ' + vehicle.coords[1], { autoPan: true });;
-            marked_vehicles.push(vehicle_id);
+            // if (vehicle_id.coords[0] < 52 && vehicle_id.coords[0] > 12 && vehicle_id.coords[1] > -128 && vehicle_id.coords[1] < -67){
+                vehicle = vehicles_dict[vehicle_id.toString()]; 
+                vehicleMarkers[vehicle_id.toString()] = L.marker(vehicle.coords, { icon: vehicle.icon }).addTo(map)
+                .bindPopup('<b>' + vehicle.name + '</b><br>Location: ' + vehicle.coords[0] + ', ' + vehicle.coords[1], { autoPan: true });;
+                marked_vehicles.push(vehicle_id);
+            // }
         }
     });
 }
@@ -85,15 +144,17 @@ function addVehicleMarkers() {
 function updateVehicleLocations() {
     latlngarray = [];
     vehicle_id_list.forEach(vehicle_id => {
-        vehicle = vehicles_dict[vehicle_id.toString()];
-        console.log(vehicleMarkers[vehicle_id.toString()]);
-        vehicleMarkers[vehicle_id.toString()].setLatLng(vehicle.coords);
-        href = 'google.com/maps/place/' + toDMS(vehicle.coords[0],true) + '' + toDMS(vehicle.coords[1],true);
-        vehicleMarkers[vehicle_id.toString()].bindPopup('<b>' + vehicle.name +
-        '</b><br>GPS Location: ' + vehicle.coords[0].toFixed(6) + ', ' + vehicle.coords[1].toFixed(6) + 
-        '<br> Altitude: ' + vehicle.alt + ' <br>' +
-        '<a class="copyLink">' + href + '</a>');
-        latlngarray.push(vehicleMarkers[vehicle_id].getLatLng());
+        // if (vehicle_id.coords[0] < 52 && vehicle_id.coords[0] > 12 && vehicle_id.coords[1] > -128 && vehicle_id.coords[1] < -67){
+            vehicle = vehicles_dict[vehicle_id.toString()];
+            console.log(vehicleMarkers[vehicle_id.toString()]);
+            vehicleMarkers[vehicle_id.toString()].setLatLng(vehicle.coords);
+            href = 'google.com/maps/place/' + toDMS(vehicle.coords[0],true) + '' + toDMS(vehicle.coords[1],true);
+            vehicleMarkers[vehicle_id.toString()].bindPopup('<b>' + vehicle.name +
+            '</b><br>GPS Location: ' + vehicle.coords[0].toFixed(6) + ', ' + vehicle.coords[1].toFixed(6) + 
+            '<br> Altitude: ' + vehicle.alt + ' <br>' +
+            '<a class="copyLink">' + href + '</a>');
+            latlngarray.push(vehicleMarkers[vehicle_id].getLatLng());
+        // }
     });
 }
 
